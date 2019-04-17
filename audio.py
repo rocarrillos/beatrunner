4342ee2894f81a459ce8694436b1f79a8181911e
Rodrigo Carrillo-Sanchez <rcarrill@mit.edu>
2019-04-17 11:26:51 -0400

    adding audio.py and common files

---
+++ audio.py
@@ -0,0 +1,11 @@
+import sys
+
+from common.core import *
+from common.gfxutil import *
+from common.audio import *
+from common.synth import *
+from common.clock import *
+
+import numpy as np
+
+print("audio exists")
